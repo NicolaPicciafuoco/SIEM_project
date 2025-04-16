a3b517a88d6ff409725d5ab0b9e901a8c6e6c5c5
gabriel <gabriel.piercecchi@gmail.com>
2025-04-16 12:23:21 +0200

    Prova regole firewall. Le crea ma non le applica. L'errore deve essere ancora trovato

--- firewall-gw/init-rules.sh
+++ firewall-gw/init-rules.sh
@@ -1,44 +1,62 @@
 #!/bin/bash
 
+# Funzione per recuperare il nome dell'interfaccia tramite l'indirizzo IP assegnato
+get_interface_by_ip() {
+    ip addr show | awk -v ip="$1" '$0 ~ ip {print $NF; exit}'
+}
+
+# Recupera le interfacce relative alle reti configurate dal docker-compose
+IF_WIFI_GUEST=$(get_interface_by_ip "172.28.10.244")
+IF_WIFI_MGMT=$(get_interface_by_ip "172.28.20.244")
+IF_ETHERNET=$(get_interface_by_ip "172.29.0.244")
+
+# L'interfaccia Internet è la stessa della route di default
+IF_INTERNET=$(ip route show default | awk '/default/ {print $5}')
+
+echo "Interfaccia wifi_guest: $IF_WIFI_GUEST"
+echo "Interfaccia wifi_mgmt: $IF_WIFI_MGMT"
+echo "Interfaccia ethernet: $IF_ETHERNET"
+echo "Interfaccia internet: $IF_INTERNET"
+
 # Abilitiamo il forwarding IP
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# Pulizia regole
+# Pulizia regole precedenti
 iptables -F
 iptables -t nat -F
 iptables -X
 
-# Politica default DROP tra le reti
+# Impostazione della politica default per il FORWARD: DROP
 iptables -P FORWARD DROP
 
-# Accettiamo loopback interno
+# Regola di logging per il traffico nella catena FORWARD
+iptables -A FORWARD -j LOG --log-prefix "IPTables-FORWARD: " --log-level 4
+
+# Accettiamo il traffico sul loopback
 iptables -A INPUT -i lo -j ACCEPT
 
-# Accettiamo traffico intra-rete (ping etc.)
-iptables -A FORWARD -i wifi_guest -o wifi_guest -j ACCEPT
-iptables -A FORWARD -i wifi_mgmt -o wifi_mgmt -j ACCEPT
-iptables -A FORWARD -i ethernet -o ethernet -j ACCEPT
+# Blocchiamo il traffico dal wifi_guest verso wifi_mgmt ed ethernet
+iptables -A FORWARD -i "$IF_WIFI_GUEST" -o "$IF_WIFI_MGMT" -j DROP
+iptables -A FORWARD -i "$IF_WIFI_GUEST" -o "$IF_ETHERNET" -j DROP
 
-# Permetti il traffico verso il server da tutte le reti
+# Blocchiamo il traffico dal wifi_mgmt verso ethernet
+iptables -A FORWARD -i "$IF_WIFI_MGMT" -o "$IF_ETHERNET" -j DROP
+
+# Accettiamo il traffico intra-rete (es. ping, traffico tra istanze sulla stessa interfaccia)
+iptables -A FORWARD -i "$IF_WIFI_GUEST" -o "$IF_WIFI_GUEST" -j ACCEPT
+iptables -A FORWARD -i "$IF_WIFI_MGMT" -o "$IF_WIFI_MGMT" -j ACCEPT
+iptables -A FORWARD -i "$IF_ETHERNET" -o "$IF_ETHERNET" -j ACCEPT
+
+# Permetti il traffico verso il server (IP 172.29.0.100) da tutte le reti
 iptables -A FORWARD -d 172.29.0.100 -j ACCEPT
 
 # Ethernet può raggiungere tutti
-iptables -A FORWARD -i ethernet -j ACCEPT
+iptables -A FORWARD -i "$IF_ETHERNET" -j ACCEPT
 
-## Permetti il traffico verso il server da tutte le reti
-#iptables -A FORWARD -d 172.29.0.100 -j ACCEPT
-
-# Blocchiamo GUEST verso mgmt o ethernet
-iptables -A FORWARD -i wifi_guest -o wifi_mgmt -j DROP
-iptables -A FORWARD -i wifi_guest -o ethernet -j DROP
-
-# Blocchiamo MGMT verso ethernet
-iptables -A FORWARD -i wifi_mgmt -o ethernet -j DROP
-
-#  NAT per accesso a internet
-iptables -t nat -A POSTROUTING -o internet -j MASQUERADE
+# NAT per accesso a internet
+iptables -t nat -A POSTROUTING -o "$IF_INTERNET" -j MASQUERADE
 
 # Permettiamo il forward verso Internet
-iptables -A FORWARD -i wifi_guest -o internet -j ACCEPT
-iptables -A FORWARD -i wifi_mgmt -o internet -j ACCEPT
-iptables -A FORWARD -i ethernet -o internet -j ACCEPT
+iptables -A FORWARD -i "$IF_WIFI_GUEST" -o "$IF_INTERNET" -j ACCEPT
+iptables -A FORWARD -i "$IF_WIFI_MGMT" -o "$IF_INTERNET" -j ACCEPT
+iptables -A FORWARD -i "$IF_ETHERNET" -o "$IF_INTERNET" -j ACCEPT
