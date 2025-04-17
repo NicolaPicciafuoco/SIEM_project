365b3ada86cbd6f28d39c2a60350568b70e54739
NicolaPicciafuoco <nicola.picciafuoco@gmail.com>
2025-04-17 09:32:21 +0200

    Routing between different interface

--- firewall-gw/init-rules.sh
+++ firewall-gw/init-rules.sh
@@ -1,44 +1,74 @@
 #!/bin/bash
 
-# Abilitiamo il forwarding IP
+#!/bin/bash
+
+# Abilita l'inoltro dei pacchetti IPv4
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# Pulizia regole
+# Pulizia delle catene esistenti
 iptables -F
 iptables -t nat -F
 iptables -X
 
-# Politica default DROP tra le reti
-iptables -P FORWARD DROP
+# Imposta la policy di default su ACCEPT (temporaneamente, poi metteremo DROP + regole specifiche)
+iptables -P FORWARD ACCEPT
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
 
-# Accettiamo loopback interno
-iptables -A INPUT -i lo -j ACCEPT
+# Permette inoltro tra tutte le interfacce
+# (se vuoi restrizioni, le mettiamo nel prossimo step)
 
-# Accettiamo traffico intra-rete (ping etc.)
-iptables -A FORWARD -i wifi_guest -o wifi_guest -j ACCEPT
-iptables -A FORWARD -i wifi_mgmt -o wifi_mgmt -j ACCEPT
-iptables -A FORWARD -i ethernet -o ethernet -j ACCEPT
 
-# Permetti il traffico verso il server da tutte le reti
-iptables -A FORWARD -d 172.29.0.100 -j ACCEPT
 
-# Ethernet può raggiungere tutti
-iptables -A FORWARD -i ethernet -j ACCEPT
 
+
+
+
+
+
+
+
+
+
+
+# Abilitiamo il forwarding IP
+#echo 1 > /proc/sys/net/ipv4/ip_forward
+#
+## Pulizia regole
+#iptables -F
+#iptables -t nat -F
+#iptables -X
+#
+## Politica default DROP tra le reti
+#iptables -P FORWARD DROP
+#
+## Accettiamo loopback interno
+#iptables -A INPUT -i lo -j ACCEPT
 ## Permetti il traffico verso il server da tutte le reti
 #iptables -A FORWARD -d 172.29.0.100 -j ACCEPT
-
-# Blocchiamo GUEST verso mgmt o ethernet
-iptables -A FORWARD -i wifi_guest -o wifi_mgmt -j DROP
-iptables -A FORWARD -i wifi_guest -o ethernet -j DROP
-
-# Blocchiamo MGMT verso ethernet
-iptables -A FORWARD -i wifi_mgmt -o ethernet -j DROP
-
-#  NAT per accesso a internet
-iptables -t nat -A POSTROUTING -o internet -j MASQUERADE
-
-# Permettiamo il forward verso Internet
-iptables -A FORWARD -i wifi_guest -o internet -j ACCEPT
-iptables -A FORWARD -i wifi_mgmt -o internet -j ACCEPT
-iptables -A FORWARD -i ethernet -o internet -j ACCEPT
+#
+## Accettiamo traffico intra-rete (ping etc.)
+#iptables -A FORWARD -i wifi_guest -o wifi_guest -j ACCEPT
+#iptables -A FORWARD -i wifi_mgmt -o wifi_mgmt -j ACCEPT
+#iptables -A FORWARD -i ethernet -o ethernet -j ACCEPT
+#iptables -A FORWARD -i wifi_guest -o ethernet -j ACCEPT
+#
+#iptables -A FORWARD -i ethernet -o wifi_guest -j ACCEPT
+## Ethernet può raggiungere tutti
+#iptables -A FORWARD -i ethernet -j ACCEPT
+#
+#
+## Blocchiamo GUEST verso mgmt o ethernet
+#iptables -A FORWARD -i wifi_guest -o wifi_mgmt -j DROP
+##iptables -A FORWARD -i wifi_guest -o ethernet -j DROP
+#
+## Blocchiamo MGMT verso ethernet
+#iptables -A FORWARD -i wifi_mgmt -o ethernet -j DROP
+#
+##  NAT per accesso a internet
+#iptables -t nat -A POSTROUTING -o internet -j MASQUERADE
+#
+## Permettiamo il forward verso Internet
+#iptables -A FORWARD -i wifi_guest -o internet -j ACCEPT
+#iptables -A FORWARD -i wifi_mgmt -o internet -j ACCEPT
+#iptables -A FORWARD -i ethernet -o internet -j ACCEPT
